37f421408982f83cf1b7f05732ab1581415253e4
litingyang <liting60231@gmail.com>
2017-03-03 21:10:15 +0800

    change structure size

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,15 +1,26 @@
 #include <stdlib.h>
-
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
     /* TODO: implement */
+    while(pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+//    e->more = (detail *) malloc(sizeof(detail));
+    e->pNext = NULL;
+
+    return e;
 }
